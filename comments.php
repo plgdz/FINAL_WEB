0befd3c3ff556e2469a9f69a57a14fdad25adbc2
plgdz <40022026+plgdz@users.noreply.github.com>
2023-11-24 22:24:49 -0500

    n

--- comments.php
+++ comments.php
@@ -1,11 +1,12 @@
 <?php
-    require_once("action/CommentsAction.php");
-    $action = new CommentsAction();
-    $data = $action->execute();
+require_once("action/CommentsAction.php");
+$action = new CommentsAction();
+$data = $action->execute();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,6 +14,7 @@
     <script src="js/comments.js"></script>
     <title>Magix Dofus</title>
 </head>
+
 <body>
     <div id="main-header">
         <h1>Notes</h1>
@@ -21,26 +23,37 @@
     <div id="main">
         <div id="comment-container">
 
-        <?php foreach($data["notes"] as $note) {?>
+            <?php foreach ($data["notes"] as $note) { ?>
 
-            <div class="comment" id="c<?= $note["id"] ?>">
-                <div class="comment-head">
-                    <div class="comment-subject"><?= $note["title"] ?></div>
-                    <div class="comment-date"><?= $note["date"] ?></div>
-                </div>
-                <div class="comment-content">
-                   <p> <?= $note["note"] ?></p>
-                </div>
-                <div class="comment-action">
-                    <div class="comment-edit"><span class="id"><?= $note["id"] ?></span></div>
-                    <div class="comment-delete">
-                        <span class="id"><?= $note["id"] ?></span>
+                <div class="comment" id="c<?= $note["id"] ?>">
+                    <div class="comment-head">
+                        <div class="comment-subject">
+                            <?= $note["title"] ?>
+                        </div>
+                        <div class="comment-date">
+                            <?= $note["date"] ?>
+                        </div>
+                    </div>
+                    <div class="comment-content">
+                        <p>
+                            <?= $note["note"] ?>
+                        </p>
+                    </div>
+                    <div class="comment-action">
+                        <div class="comment-edit"><span class="id">
+                                <?= $note["id"] ?>
+                            </span></div>
+                        <div class="comment-delete">
+                            <span class="id">
+                                <?= $note["id"] ?>
+                            </span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        
-        <?php } ?>
+
+            <?php } ?>
         </div>
     </div>
 </body>
+
 </html>
