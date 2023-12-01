ebed674e42abcff6474ede22ceac0ae0cda6dc9f
plgdz <40022026+plgdz@users.noreply.github.com>
2023-12-01 11:03:08 -0500

    back button
    from notes to lobby - anim css

--- comments.php
+++ comments.php
@@ -16,6 +16,27 @@ $data = $action->execute();
 </head>
 
 <body>
+    <div id="back">
+        <!-- https://codepen.io/shunyadezain/pen/rNWawyP -->
+        <a href="lobby.php" class="link">
+            <div class="link-icon">
+                <svg class="icon" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd"
+                    clip-rule="evenodd">
+                    <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
+                </svg>
+                <svg class="icon" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd"
+                    clip-rule="evenodd">
+                    <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
+                </svg>
+            </div>
+            <span class="mask">
+                <div class="link-container">
+                    <span class="link-title1 title">RETOUR</span>
+                    <span class="link-title2 title">RETOUR</span>
+                </div>
+            </span>
+        </a>
+    </div>
     <div id="main-header">
         <h1>Notes</h1>
         <div id="new"></div>
